0dd37d2bb8aedaaf2bda4740bcacc9a6d5bba4f4
Sierk <sierkje@users.noreply.github.com>
2018-06-13 15:16:16 -0700

    Fixed iterate.js
    Problem was that:
    
    - if the key is in the "top" object/array, the object was returned; 
    - if the key is in a "child" object of the "top" object/array, the child object was not returned.
    
    This should solve things, I did test this with those 4 use cases - it seems to work. (Also tested for the key not being present, in that case things resolve to _undefined_.)
    
    Test code:
    ```
    async function iterateObj(key, obj){
    
    		if(isObject(obj)){
    			//console.log('Object');
    			let childObjectKeys = Object.keys(obj);
    			//console.log(childObjectKeys);
    			for(let i = 0; i < childObjectKeys.length; i++){
    				let childObjectKey = childObjectKeys[i];
    				let childObject = obj[childObjectKey];
    				if(key === childObjectKey){
    					console.log('found');
    					console.log(childObject);
    					return childObject;
    			
    					//console.log(obj[childObjectKey]);
    					//console.log(childObject);
    					
    				} else {
    					return await iterateObj(key, childObject);
    				}
    			}
    		} else if(isArray(obj)){
    			//console.log('Array');
    			for(let i = 0; i < obj.length; i++){
    				let childObject = obj[i];
    				return await iterateObj(key, childObject);	
    			}
    		} 
    	
    }
    
    function isArray(a){
    	return (!!a) && (a.constructor === Array);
    }
    
    function isObject(a){
    	return (!!a) && (a.constructor === Object);
    }
    
    // Object
    iterateObj('not found', {
      hello: { goodbye: { found: { result: 'found object' } } }
    }).then (x => console.log('exec', x))
    iterateObj('found', {
      hello: { goodbye: { found: { result: 'found object' } } }
    }).then (x => console.log('exec', x))
    
    // Array
    iterateObj('found', [
      [{ hello: [{ found: { result: 'found array' } }] }]
    ]).then (x => console.log('exec', x))
    iterateObj('not found', [
      [{ hello: [{ found: { result: 'not found array' } }] }]
    ]).then (x => console.log('exec', x))
    ```

--- iterate.js
+++ iterate.js
@@ -24,14 +24,14 @@ async function iterateObj(key, obj){
 					//console.log(childObject);
 					
 				} else {
-					await iterateObj(key, childObject);
+					return await iterateObj(key, childObject);
 				}
 			}
 		} else if(isArray(obj)){
 			//console.log('Array');
 			for(let i = 0; i < obj.length; i++){
 				let childObject = obj[i];
-				await iterateObj(key, childObject);	
+				return await iterateObj(key, childObject);	
 			}
 		} 
 	
